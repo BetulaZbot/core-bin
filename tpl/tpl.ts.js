d6032ac9c8e4f42c014755d14f566a983c307d88
BetulaZbot <37165016+BetulaZbot@users.noreply.github.com>
2020-05-14 14:10:48 +0800

    Add files via upload

--- tpl/tpl.ts.js
+++ tpl/tpl.ts.js
@@ -21,11 +21,11 @@ module.exports = (name) => {
             type: 'folder',
             content: [
                 {
-                    name: 'index.js',
+                    name: 'index.ts',
                     type: 'file',
                     tpl: `import { db } from 'core-dbjs'
 class InitState extends db {
-    constructor(name, updateView) {
+    constructor(name:string, updateView:Function) {
         super(name, updateView);
         //声明所属的表
         this.initTables([])
@@ -40,12 +40,12 @@ export default InitState`
             type: 'folder',
             content: [
                 {
-                    name: 'index.js',
+                    name: 'index.tsx',
                     type: 'file',
                     tpl: `
 import './index.scss'
 import React, { Component } from 'react'
-export default class extends Component {
+export default class extends Component<any, any> {
     constructor(props) {
         super(props);
     }
@@ -65,9 +65,9 @@ export default class extends Component {
             type: 'folder',
             content: [
                 {
-                    name: 'main.js',
+                    name: 'main.ts',
                     type: 'file',
-                    tpl: `import { Worker, recordRst } from 'core-element'   
+                    tpl: `import { Worker , recordRst } from 'core-element'   
 export default class extends Worker {
     @recordRst()
     getHandler(handler){
@@ -81,9 +81,9 @@ export default class extends Worker {
             type: 'file',
             tpl: `import db from './var/index'
 import $platform from '../../client'
-import main from './worker/main'
+import main from './worker/main.ts'
 import * as config from './config'
-import view from './view'
+import view from './view/index.tsx'
 import getElement from 'core-manager'
 const name = '${name}'
 export default getElement(name, view, db, { main }, $platform, config)`
